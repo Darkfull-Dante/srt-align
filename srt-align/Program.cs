c64545bc233f5924f8760dd1db3ab5cab528cd9c
Felix Cusson <felixcusson@gmail.com>
2021-06-10 08:37:05 -0400

    Add project files.

---
+++ srt-align/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace srt_align
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
